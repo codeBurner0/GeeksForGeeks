035d264d351d381c438ac001b7e2bd262a0d940a
CodeBurner <codeburner0@gmail.com>
2022-12-30 17:10:29 +0530

    new update

--- BitManipulation/MissingNumber.java
+++ BitManipulation/MissingNumber.java
@@ -1,15 +1,23 @@
 package BitManipulation;
 
+import java.util.Arrays;
+
 public class MissingNumber {
     public static int Number(int arr[]) {
+        Arrays.sort(arr);
         int res=0;
+        int one=0;
+        int two=0;
         for(int i=0;i<arr.length;i++){
             res=res^arr[i]^(i+1);
+            if(i%2==0){
+                
+            }
         }
         return res^(arr.length+1);
     }
     public static void main(String[] args) {
-        int arr[]={1,2,3,8,6,7,4};
+        int arr[]={1,2,3,4,5,3,2,1,1,1};
         System.out.println(Number(arr));
     }
 }
