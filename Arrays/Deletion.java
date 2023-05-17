4544e0e444513f3e0232bc1bf28e83aa20d9df8a
CodeBurner <codeburner0@gmail.com>
2023-05-17 21:21:51 +0530

    Deletion

--- Arrays/Deletion.java
+++ Arrays/Deletion.java
@@ -1,3 +1,17 @@
+import java.util.Arrays;
+
 public class Deletion {
-    
+    public static void del(int[] arr, int pos) {
+            for(int i=pos;i<arr.length-1;i++){
+                arr[i]=arr[i+1];
+            }
+            arr[arr.length-1]=0;
+        
+}
+    public static void main(String[] args) {
+        int[] arr={1,2,0,3,8,5,6,0};
+       del(arr, 2);
+       del(arr, 0);
+        System.out.println(Arrays.toString(arr));
+    }
 }
