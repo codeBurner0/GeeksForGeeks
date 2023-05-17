154acbc3cb7d85bc9d86ff52be1c798d8a2cbdee
CodeBurner <codeburner0@gmail.com>
2023-05-17 22:44:43 +0530

    RemoveDuplicates

---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+
+    public static void rdup(int[] arr) {
+        int count=1;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]!=arr[i+1]){
+                arr[count]=arr[i+1];
+                count++;
+            }
+        }
+        int j=arr.length-1;
+        int start=arr.length-1-count;
+        while(start>=0){
+            arr[j]=0;
+            j--;
+            start--;
+        }
+        
+    }
+    public static void main(String[] args) {
+        int[] arr={22,4,3,3,3,3,5,5,56,6,7,7,9,8};
+        rdup(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
